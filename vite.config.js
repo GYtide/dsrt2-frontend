4db6f966e23b71f8987fe525a94922510ac60dea
gytide <2142780749@qq.com>
2023-03-09 21:39:44 +0800

    配置路径别名

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // 约定：使用 @ 表示 src 文件所在路径
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
