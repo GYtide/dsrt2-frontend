d7a96173d6127f3694c10231506d1db81486c742
gytide <2142780749@qq.com>
2023-03-10 22:04:42 +0800

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import './App.css'
 import { observer } from 'mobx-react-lite'
 import { useStore } from './store/index'
 
+import { Router, Route, Routes, BrowserRouter } from 'react-router-dom'
+
+import GeekLayout from './components/Layout'
 function App() {
   const { counterStore } = useStore()
-  console.log(counterStore)
-  return <div className="App"></div>
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<GeekLayout />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default observer(App)
- 
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,11 +1,12 @@
 import { Layout, Menu, Popconfirm } from 'antd'
-
 import {
   HomeOutlined,
   DiffOutlined,
   EditOutlined,
   LogoutOutlined,
 } from '@ant-design/icons'
+import './index.scss'
+
 // 解构 antd Layout
 const { Header, Sider } = Layout
 
@@ -37,7 +38,7 @@ const GeekLayout = () => {
               内容管理
             </Menu.Item>
             <Menu.Item icon={<EditOutlined />} key="3">
-              发布文章 
+              发布文章
             </Menu.Item>
           </Menu>
         </Sider>
@@ -48,3 +49,5 @@ const GeekLayout = () => {
     </Layout>
   )
 }
+
+export default GeekLayout
