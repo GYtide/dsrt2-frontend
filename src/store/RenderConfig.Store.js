ca5dce176f5005277c9e07d3ca7542bdaceabd57
gytide <2142780749@qq.com>
2023-03-12 22:27:18 +0800

    渲染参数状态

---
+++ src/store/RenderConfig.Store.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ * 对数据的渲染参数
+ *
+ */
+
+class RenderConfigStore {
+
+  //  可选的colorMap
+  COLOR_MAPS_SELECTED = [
+    "gray",
+    "afmhot",
+    "Blues",
+    "coolwarm",
+    "cubehelix",
+    "gist_heat",
+    "gist_stern",
+    "gnuplot",
+    "gnuplot2",
+    "greens",
+    "greys",
+    "hot",
+    "inferno",
+    "jet",
+    "magma",
+    "nipy_spectral",
+    "plasma",
+    "rainbow",
+    "RdBu",
+    "RdGy",
+    "reds",
+    "seismic",
+    "spectral",
+    "tab10",
+    "viridis"
+  ];
+  stokesIndex = 0
+  inverted = false // boolean; 是否反色
+
+}
