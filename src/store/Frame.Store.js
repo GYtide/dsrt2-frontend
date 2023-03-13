c5a80dbd636631ac6aed4f6079387b07e7abf5ce
gytide <2142780749@qq.com>
2023-03-13 09:28:58 +0800

    返回canvas对象

--- src/store/Frame.Store.js
+++ src/store/Frame.Store.js
@@ -19,9 +19,33 @@ class FrameStore {
     // 用于显示的canvas对象以及其上下文
     this.canvas = document.createElement('canvas')
     this.context = this.canvas.getContext('2d')
+    this.context.scale(1, -1)
+    // 使用translate()函数调整y轴位置
+    this.context.translate(0, canvas.height)
   }
 
-  
+  get canvasInstance () {
+
+    const normalized = this.frameData.map(value => (value - min) / (max - min))
+
+    // 缩放至0到255之间
+    const scaled = normalized.map(value => Math.round(value * 255))
+    var rasterdata = []
+
+    for (let i = 0; i < this.frameData.length; ++i) {
+      rasterdata[4 * i] = scaled[i]
+      rasterdata[4 * i + 1] = scaled[i]
+      rasterdata[4 * i + 2] = scaled[i]
+      rasterdata[4 * i + 3] = 255
+    }
+    var imageData = this.context.getImageData(0, 0, canvas.width, canvas.height)
+    imageData.data.set(rasterdata)
+    this.context.putImageData(imageData, 0, 0)
+
+    return this.canvas
+
+  }
+
 
 }
 
